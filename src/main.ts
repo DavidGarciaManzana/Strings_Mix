32733d05ff462a028b173ab9aa51a2c653563d66
David Garcia <Davidgarciama@outlook.com>
2023-02-19 12:04:14 -0600

    Second step

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,3 @@
-// Given two strings s1 and s2, we want to visualize how different the two strings are. We will only take into account the lowercase letters (a to z). First let us count the frequency of each lowercase letters in s1 and s2.
-
-// s1 = "A aaaa bb c"
-
-// s2 = "& aaa bbb c d"
-
-// s1 has 4 'a', 2 'b', 1 'c'
-
-// s2 has 3 'a', 3 'b', 1 'c', 1 'd'
-
-// So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2. In the following we will not consider letters when the maximum of their occurrences is less than or equal to 1.
-
-// We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb" where 1 in 1:aaaa stands for string s1 and aaaa because the maximum for a is 4. In the same manner 2:bbb stands for string s2 and bbb because the maximum for b is 3.
-
-// The task is to produce a string in which each lowercase letters of s1 or s2 appears as many times as its maximum if this maximum is strictly greater than 1; these letters will be prefixed by the number of the string where they appear with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:.
-
-// In the result, substrings (a substring is for example 2:nnnnn or 1:hhh; it contains the prefix) will be in decreasing order of their length and when they have the same length sorted in ascending lexicographic order (letters and digits - more precisely sorted by codepoint); the different groups will be separated by '/'. See examples and "Example Tests".
-
-// Hopefully other examples can make this clearer.
-
-// s1 = "my&friend&Paul has heavy hats! &"
-// s2 = "my friend John has many many friends &"
-// mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
-
-// s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
-// s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
-// mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
-
-// s1="Are the kids at home? aaaaa fffff"
-// s2="Yes they are here! aaaaa fffff"
-// mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
-// Note for Swift, R, PowerShell
-// The prefix =: is replaced by E:
-
-// s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
-// s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
-// mix(s1, s2) --> "1:mmmmmm/E:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/E:ee/E:ss"
-
 let mix = (s1: string, s2: string): string => {
     //1.-Create 2 objects to save the strings without spaces and other characters
     let obj1: { [key: string]: number } = {}
@@ -46,14 +8,50 @@ let mix = (s1: string, s2: string): string => {
         if (/^[a-z]+$/.test(letter)) {
             //4.-Insert every letter to the object to know how many of that letters the string has
             //**Using the if just for the first time 
-            if(!obj1[letter]){
+            if (!obj1[letter]) {
                 obj1[letter] = 1
-            }else {
+            } else {
                 obj1[letter] += 1
             }
         }
 
-    }console.log(obj1)
+    }
+    //5.-The same for the second string/object
+    for (let letter of s2) {
+        if (/^[a-z]+$/.test(letter)) {
+            if (!obj2[letter]) {
+                obj2[letter] = 1
+            } else {
+                obj2[letter] += 1
+            }
+        }
+
+    }
+    //6.-Get a new object with only the letters with the greater number (do not take numbers equal or below 1)
+    let objFusion: { [key: string]: number } = {}
+
+    function getHighestValues(objFusion: any, obj1: any, obj2: any): any {
+        //7.-Using the spread operator you can create an objectFusion with all the data 
+        //from obj1 and obj2, then you use the Object.keys to obtein only the letters
+        const letters = Object.keys({ ...obj1, ...obj2 });
+        //8.- For each letter you look into both objects, and save the value in each constant
+        //(Only if the value is above 1)
+        let value1: number;
+        let value2: number;
+        letters.forEach((letter) => {
+            if (obj1[letter] && obj1[letter]>1) { value1 = obj1[letter] } else {
+                value1 = 0
+            }
+            if (obj2[letter] && obj2[letter]>1) { value2 = obj2[letter] } else {
+                value2 = 0
+            }
+            //10.-Using the Math.max you get only the higher value and saved it the objFusion
+            objFusion[letter] = Math.max(value1, value2);
+        });
+        return objFusion
+    }
+    objFusion = getHighestValues(objFusion, obj1, obj2)
+    console.log(objFusion)
     return ""
 }
 
